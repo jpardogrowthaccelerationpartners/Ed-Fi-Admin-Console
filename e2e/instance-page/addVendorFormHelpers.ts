b57f02288b29a3e0a21df84bcd4c3e08d7352014
Danny Fernandez A <129120407+dfernandez-gap@users.noreply.github.com>
2025-05-19 21:11:06 -0600

    Update addVendorFormHelpers.ts

--- e2e/instance-page/addVendorFormHelpers.ts
+++ e2e/instance-page/addVendorFormHelpers.ts
@@ -29,8 +29,7 @@ const fillAddVendorPartnerForm = async ({ page, vendorName, company, contactEmai
   if (nameSpacePrefixes && nameSpacePrefixes.length > 0) {
     for (const prefix of nameSpacePrefixes) {
       await page.getByLabel('Add Namespace Prefixes').type(prefix, { delay: 100 });
-      //await page.getByLabel('Add Namespace Prefixes').fill(prefix)
-      await page.getByText(prefix, { exact: true }).click()
+      await page.locator('section[role="dialog"]').getByText(prefix, { exact: true }).click()
     }
   }
 }
